101fb36e041a755c26f77ebdfb4e43de5d8926ad
Dayanna García <dayanna_garcia@hotmail.com>
2022-03-28 08:47:07 -0400

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,81 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import React, { useState } from "react";
+import ListaTareas from "./tasklist.jsx";
 //create your first component
+
 const Home = () => {
+	const [task, setTask] = useState("");
+	const [taskList, setTaskList] = useState([]);
+	const [error, setError] = useState(false);
+	const [contador, setcontador] = useState("");
+
+	const handleChangeTask = (event) => {
+		setTask(event.target.value);
+	};
+
+	const handleAddTask = () => {
+		if (task.trim() != "") {
+			setTaskList([...taskList, task]);
+			setTask("");
+			setError(false);
+			setcontador(taskList.length + 1);
+		} else {
+			setError(true);
+		}
+	};
+
+	const handleDelete = (id) => {
+		let newListTask = taskList.filter((item, index) => index != id);
+
+		setcontador(newListTask.length);
+		setTaskList(newListTask);
+	};
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="container mt-4 letter ">
+				<div className="row justify-content-center fw-light">
+					<h1 className=" title text-center ">todos</h1>
+					<div className="col-12 col-md-6 shadow p-3 mb-5 bg-body rounded">
+						<form>
+							<div className="form-group d-flex">
+								<div className="w-100 me-2">
+									<input
+										type="text"
+										className="form-control border-0"
+										name="nametask"
+										placeholder="What needs to be done?"
+										value={task}
+										onChange={handleChangeTask}
+									/>
+								</div>
+								<div className="me-3">
+									<button
+										className="btn btn-outline-success"
+										type="button"
+										onClick={handleAddTask}>
+										<i className="fas fa-check"></i>
+									</button>
+								</div>
+							</div>
+						</form>
+						{error && (
+							<div className="alert alert-secondary my-2 p-1 w-50">
+								"No hay tarea, agregue una tarea"
+							</div>
+						)}
+						<ListaTareas
+							taskList={taskList}
+							handleDelete={handleDelete}
+							contador={contador}
+						/>
+						{contador != 0 && (
+							<div className="mt-2 contador">
+								{contador} items left
+							</div>
+						)}
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
---
+++ src/js/component/tasklist.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ListaTareas = ({ taskList, handleDelete, contador }) => {
+	return (
+		<>
+			{taskList.map((item, index) => (
+				<div key={index} className="border-bottom w-100 mt-2">
+					<ul className="list-group list-group-flush vertical-align">
+						<li className="list-group-item d-flex justify-content-between">
+							{item}
+							<button
+								type="button"
+								className="eliminar btn btn-light mt-2 px-3 py-1"
+								onClick={() => handleDelete(index)}>
+								<i className="fas fa-times"></i>
+							</button>
+						</li>
+					</ul>
+				</div>
+			))}
+		</>
+	);
+};
+
+ListaTareas.propTypes = {
+	taskList: PropTypes.array,
+	handleDelete: PropTypes.func,
+	contador: PropTypes.number,
+};
+
+export default ListaTareas;
